a78f3b906a8bb0091bbeed72954901c94aba7b10
Kimmo Salmela <kimmo.salmela@reun.eu>
2025-07-09 11:57:13 +0300

    Normalize trailing slashes

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,12 +2,13 @@
 import { defineConfig } from "astro/config";
 
 import tailwindcss from "@tailwindcss/vite";
+import normalizeTrailingSlash from "@reunmedia/astro-normalize-trailing-slash";
 
 // https://astro.build/config
 export default defineConfig({
   site: "https://cryszon.github.io",
   trailingSlash: "always",
-
+  integrations: [normalizeTrailingSlash()],
   vite: {
     plugins: [tailwindcss()],
   },
