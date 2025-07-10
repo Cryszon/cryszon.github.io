dc011d1c34cbaf0fdbc4be20125a8ed35a3237a3
Kimmo Salmela <kimmo.salmela@reun.eu>
2025-07-10 13:39:18 +0300

    Add ESLint with @reunmedia/eslint-config

---
+++ eslint.config.mjs
@@ -0,0 +1,5 @@
+// @ts-check
+import tseslint from "typescript-eslint";
+import createReunMediaConfig from "@reunmedia/eslint-config";
+
+export default tseslint.config(await createReunMediaConfig(import.meta.url));
